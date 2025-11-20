2af2e67d92654c516c88473dfa1eca08ae335709
Samuel Sendek <cooperavalanche@gmail.com>
2025-11-20 18:10:44 +0100

    Working towards monitor process
    Trying to create a monitor process for our main process.
    
    Co-Authored-By: Lukas Lannge <48261617+MT0DE@users.noreply.github.com>

--- s3k/projects/malloc/app0/main.c
+++ s3k/projects/malloc/app0/main.c
@@ -1,12 +1,22 @@
 #include "altc/altio.h"
 #include "s3k/s3k.h"
-#include "canary.h"
 #include <string.h>
+#include "../../tutorial-commons/utils.h"
 
-#include "malloc.h"
+#include "../app1/malloc.h"
 
 #define APP0_PID 0
+#define APP0_BASE_ADDR 0x80010000
+#define APP0_LENGHT 0x10000
+
 #define APP1_PID 1
+#define APP1_BASE_ADDR 0x80020000
+#define APP1_LENGHT 0x10000
+
+#define APP2_PID 2
+#define APP2_BASE_ADDR 0x80030000
+#define APP2_LENGHT 0x10000
+
 
 // See plat_conf.h
 #define BOOT_PMP 0
@@ -20,11 +30,9 @@
 #define MONITOR 8
 #define CHANNEL 9
 
-extern int __heap_pointer;
-extern int __canary_pointer;
 extern int _end;
 
-void setup_uart(uint64_t uart_idx)
+void setup_uart1(uint64_t uart_idx)
 {
 	uint64_t uart_addr = s3k_napot_encode(UART0_BASE_ADDR, 0x8);
 	// Derive a PMP capability for accessing UART
@@ -36,60 +44,85 @@ void setup_uart(uint64_t uart_idx)
 	s3k_sync_mem();
 }
 
-void setup_app1(uint64_t tmp)
+void setup_app1()
 {
 	uint64_t uart_addr = s3k_napot_encode(UART0_BASE_ADDR, 0x8);
-	uint64_t app1_addr = s3k_napot_encode(0x80020000, 0x10000);
+	uint64_t app1_addr = s3k_napot_encode(APP1_BASE_ADDR, APP1_LENGHT);
+	uint64_t app2_addr = s3k_napot_encode(APP2_BASE_ADDR, APP2_LENGHT);
+	uint64_t app1_heap_addr = s3k_napot_encode((uint64_t)&__heap_pointer, (uint64_t)&__heap_size);
 
 	// Derive a PMP capability for app1 main memory
-	s3k_cap_derive(RAM_MEM, tmp, s3k_mk_pmp(app1_addr, S3K_MEM_RWX));
-	s3k_mon_cap_move(MONITOR, APP0_PID, tmp, APP1_PID, 0);
-	s3k_mon_pmp_load(MONITOR, APP1_PID, 0, 0);
+	uint32_t free_cap_idx = find_free_cap();
+	s3k_err_t a1 = s3k_cap_derive(RAM_MEM, free_cap_idx, s3k_mk_pmp(app1_addr, S3K_MEM_RWX));
+	s3k_err_t a2 = s3k_mon_cap_move(MONITOR, APP0_PID, free_cap_idx, APP1_PID, 0);
+	s3k_err_t a3 = s3k_mon_pmp_load(MONITOR, APP1_PID, 0, 0);
+	
+	// Derive a PMP capability for app2 main memory
+	s3k_err_t a4 = s3k_cap_derive(RAM_MEM, free_cap_idx, s3k_mk_pmp(app2_addr, S3K_MEM_RWX));
+	s3k_err_t a5 = s3k_mon_cap_move(MONITOR, APP0_PID, free_cap_idx, APP2_PID, 0);
+	s3k_err_t a6 = s3k_mon_pmp_load(MONITOR, APP2_PID, 0, 0);
+
+	
+	
+	// Derive a PMP capability for uart
+	s3k_err_t b1 = s3k_cap_derive(UART_MEM, free_cap_idx, s3k_mk_pmp(uart_addr, S3K_MEM_RW));
+	s3k_err_t b2 = s3k_mon_cap_move(MONITOR, APP0_PID, free_cap_idx, APP1_PID, 1);
+	s3k_err_t b3 = s3k_mon_pmp_load(MONITOR, APP1_PID, 1, 1);
 
 	// Derive a PMP capability for uart
-	s3k_cap_derive(UART_MEM, tmp, s3k_mk_pmp(uart_addr, S3K_MEM_RW));
-	s3k_mon_cap_move(MONITOR, APP0_PID, tmp, APP1_PID, 1);
-	s3k_mon_pmp_load(MONITOR, APP1_PID, 1, 1);
+	s3k_err_t b4 = s3k_cap_derive(UART_MEM, free_cap_idx, s3k_mk_pmp(uart_addr, S3K_MEM_RW));
+	s3k_err_t b5 = s3k_mon_cap_move(MONITOR, APP0_PID, free_cap_idx, APP2_PID, 1);
+	s3k_err_t b6 = s3k_mon_pmp_load(MONITOR, APP2_PID, 1, 1);
 
-	// derive a time slice capability
-	s3k_mon_cap_move(MONITOR, APP0_PID, HART1_TIME, APP1_PID, 2);
+	if (a1 != S3K_SUCCESS || a2 != S3K_SUCCESS || a3 != S3K_SUCCESS ||
+		a4 != S3K_SUCCESS || a5 != S3K_SUCCESS || a6 != S3K_SUCCESS ||
+		b1 != S3K_SUCCESS || b2 != S3K_SUCCESS || b3 != S3K_SUCCESS ||
+		b4 != S3K_SUCCESS || b5 != S3K_SUCCESS || b6 != S3K_SUCCESS){
+		alt_printf("Failed app1/app2 PMP setup\n");
+		alt_printf("a1: %d, a2: %d, a3: %d\n", a1, a2, a3);
+		alt_printf("a4: %d, a5: %d, a6: %d\n", a4, a5, a6);
+		alt_printf("b1: %d, b2: %d, b3: %d\n", b1, b2, b3);
+		alt_printf("b4: %d, b5: %d, b6: %d\n", b4, b5, b6);
+	}
 
-	// Write start PC of app1 to PC
+	// derive a time slice capability app1
+	s3k_err_t c1 = s3k_mon_cap_move(MONITOR, APP0_PID, HART1_TIME, APP1_PID, 3);
+	// derive a time slice capability app2
+	s3k_err_t c4 = s3k_mon_cap_move(MONITOR, APP0_PID, HART2_TIME, APP2_PID, 3);
+
+
+	// // derive a new monitor over app1 and app2 for app1
+	// s3k_err_t worked_init = s3k_cap_derive(MONITOR, free_cap_idx, s3k_mk_monitor(APP0_PID, APP1_PID));
+	// s3k_err_t worked = s3k_mon_cap_move(MONITOR, APP0_PID, free_cap_idx, APP1_PID, 8);
+	
+
+
+	// if (worked_init != S3K_SUCCESS || worked != S3K_SUCCESS){
+	// 	alt_printf("Failed worked_init: error %d\n", worked_init);
+	// 	alt_printf("Failed worked: error %d\n", worked);	
+	// }
+
+	// Write start PC of app1 and app2 to PC
 	s3k_mon_reg_write(MONITOR, APP1_PID, S3K_REG_PC, 0x80020000);
+	s3k_mon_reg_write(MONITOR, APP2_PID, S3K_REG_PC, 0x80030000);
 
-	// Start app1
+	// Start app1 and app2
 	s3k_mon_resume(MONITOR, APP1_PID);
+	s3k_mon_resume(MONITOR, APP2_PID);
 }
 
 int main(void)
 {
 	// Setup UART access
-	setup_uart(10);
+	setup_uart1(10);
+	alt_printf("hello from app0\n");
 
-	s3k_init_malloc();
-	
-	char* dynamic_ints_a = s3k_simple_malloc(10); // 10 104+90 = 194
-	print_malloc_debug_info("--- BLOCKS AFTER A ---");
-	char* dynamic_ints_b = s3k_simple_malloc(200*sizeof(char));
-	print_malloc_debug_info("--- BLOCKS AFTER B ---");
-	s3k_simple_free(dynamic_ints_b);
-	int* dynamic_ints_c = s3k_simple_malloc(4*sizeof(int));
-	print_malloc_debug_info("--- BLOCKS AFTER C ---");
+	// s3k_init_malloc();
 
-	alt_printf("Position of dyn int a: 0x%x\n\n", dynamic_ints_a);
-	alt_printf("Position of dyn int b: 0x%x\n\n", dynamic_ints_b);
-	alt_printf("Position of dyn int c: 0x%x\n\n", dynamic_ints_c);
-
-
-    alt_printf("Canary metadata pointer 0x%x\n", &__canary_metadata_pointer);
-	
-	*(&__canary_metadata_pointer + 984) = 3;
-	*(&__canary_metadata_pointer + 1484) = 2;
-
-	init_canary_table();
-	add_canary((uint64_t*) (&__canary_metadata_pointer + 984));
-	add_canary((uint64_t*) (&__canary_metadata_pointer + 1484));
-	check_canary();
-	remove_canary((uint64_t*) (&__canary_metadata_pointer + 1484));
-	// read_canary(0);
+	setup_app1();
+	alt_printf("leaving app0\n");
+	// alt_printf("Address for heap of app1: 0x%x", &__heap_pointer);
 }
+
+
+
---
+++ s3k/projects/malloc/app2/main.c
@@ -0,0 +1,10 @@
+#include "altc/altio.h"
+#include "s3k/s3k.h"
+#include <string.h>
+#include "../../tutorial-commons/utils.h"
+
+int main(void)
+{
+    alt_printf("APP2 PRINTS\n");
+    return 0;
+}
